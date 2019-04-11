c9227c6cd4d91581123458881238c47c9f551619
Myller Claudino <myllercf@gmail.com>
2019-04-11 14:26:02 -0300

    teste tabela com dados geometricos

---
+++ teste criacao de tabela.sql
@@ -0,0 +1,12 @@
+CREATE TABLE tabela_dados_geometricos (
+	id INTEGER
+	, nome VARCHAR
+	, ponto POINT
+	, linha LINE
+	, segmento LSEG
+	, caixa BOX
+	, caminho PATH
+	, poligono POLYGON
+	, circulo CIRCLE
+	, geometria GEOMETRY
+);
